14ec5294f5562a12cd7590b123fed6aa5e9f5aee
caramezr <rebeca.caramez-saraiva-santos@grenoble-inp.org>
2020-09-23 17:47:49 +0200

    Creation du module responsable pour le graphique type camembert

---
+++ scripts/pieChart.js
@@ -0,0 +1,7 @@
+/**
+ * Module permettant de générer un graphique du type camembert
+ * des percentages des émissions de gaz à effet de serre selon les
+ * 7 secteurs et les 5 continents.
+ *
+ * 2020 - Rebeca CARAMEZ
+ */
