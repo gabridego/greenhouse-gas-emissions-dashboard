3e0442c008cffb62fc99043e29be427302284576
caramezr <rebeca.caramez-saraiva-santos@grenoble-inp.org>
2020-09-25 11:02:00 +0200

    secteurs and continents for pie chart

--- scripts/pieChart.js
+++ scripts/pieChart.js
@@ -12,13 +12,13 @@
     type: 'multilevelpie',
     renderAt: 'piechart',
     id: "myChart",
-    width: '500',
-    height: '500',
+    width: '700',
+    height: '700',
     dataFormat: 'json',
     dataSource: {
       "chart": {
-        "caption": "Sales Breakup - Top Product Categories",
-        "subcaption": "Last Quarter",
+        "caption": "Émissions de GHG selon les secteurs et les continents",
+        "subcaption": "Year : 1990",
         "showPlotBorder": "1",
         "piefillalpha": "60",
         "pieborderthickness": "2",
@@ -29,87 +29,191 @@
         "theme": "fusion"
       },
       "category": [{
-        "label": "Products",
+        "label": "Secteurs",
         "color": "#ffffff",
         "value": "150",
         "category": [{
-          "label": "Food & {br}Beverages",
+          "label": "Agriculture",
           "color": "#f8bd19",
           "value": "55.5",
           "tooltext": "Food & Beverages, $$valueK, $percentValue",
           "category": [{
-            "label": "Breads",
+            "label": "EU",
             "color": "#f8bd19",
             "value": "11.1"
           }, {
-            "label": "Juice",
+            "label": "AF",
             "color": "#f8bd19",
             "value": "27.75"
           }, {
-            "label": "Noodles",
+            "label": "AS",
             "color": "#f8bd19",
             "value": "9.99"
           }, {
-            "label": "Seafood",
+            "label": "NA/SA",
             "color": "#f8bd19",
             "value": "6.66"
+          }, {
+            "label": "OC",
+            "color": "#f8bd19",
+            "value": "9.99"
           }]
         }, {
-          "label": "Apparel &{br}Accessories",
+          "label": "Énergie",
           "color": "#33ccff",
           "value": "42",
           "tooltext": "Apparel & Accessories, $$valueK, $percentValue",
           "category": [{
-            "label": "Sun Glasses",
+            "label": "EU",
             "color": "#33ccff",
             "value": "10.08"
           }, {
-            "label": "Clothing",
+            "label": "AF",
             "color": "#33ccff",
             "value": "18.9"
           }, {
-            "label": "Handbags",
+            "label": "AS",
             "color": "#33ccff",
             "value": "6.3"
           }, {
-            "label": "Shoes",
+            "label": "NA/SA",
+            "color": "#33ccff",
+            "value": "6.72"
+          }, {
+            "label": "OC",
             "color": "#33ccff",
             "value": "6.72"
           }]
         }, {
-          "label": "Baby {br}Products",
+          "label": "Industrie",
           "color": "#ffcccc",
           "value": "22.5",
           "tooltext": "Baby Products, $$valueK, $percentValue",
           "category": [{
-            "label": "Bath &{br}Grooming",
+            "label": "EU",
             "color": "#ffcccc",
             "value": "9.45",
             "tooltext": "Bath & Grooming, $$valueK, $percentValue"
           }, {
-            "label": "Food",
+            "label": "AF",
             "color": "#ffcccc",
             "value": "6.3"
           }, {
-            "label": "Diapers",
+            "label": "AS",
+            "color": "#ffcccc",
+            "value": "6.75"
+          }, {
+            "label": "NA/SA",
+            "color": "#ffcccc",
+            "value": "6.75"
+          }, {
+            "label": "OC",
             "color": "#ffcccc",
             "value": "6.75"
           }]
         }, {
-          "label": "Electronics",
+          "label": "Chaleur & {br}Electricité",
+          "color": "##696969",
+          "value": "22.5",
+          "tooltext": "Baby Products, $$valueK, $percentValue",
+          "category": [{
+            "label": "EU",
+            "color": "##696969",
+            "value": "9.45",
+            "tooltext": "Bath & Grooming, $$valueK, $percentValue"
+          }, {
+            "label": "AF",
+            "color": "##696969",
+            "value": "6.3"
+          }, {
+            "label": "AS",
+            "color": "##696969",
+            "value": "6.75"
+          }, {
+            "label": "NA/SA",
+            "color": "##696969",
+            "value": "6.75"
+          }, {
+            "label": "OC",
+            "color": "##696969",
+            "value": "6.75"
+          }]
+        }, {
+          "label": "Énergie de {br}Construction",
+          "color": "##FF0000",
+          "value": "22.5",
+          "tooltext": "Baby Products, $$valueK, $percentValue",
+          "category": [{
+            "label": "EU",
+            "color": "##FF0000",
+            "value": "9.45",
+            "tooltext": "Bath & Grooming, $$valueK, $percentValue"
+          }, {
+            "label": "AF",
+            "color": "##FF0000",
+            "value": "6.3"
+          }, {
+            "label": "AS",
+            "color": "##FF0000",
+            "value": "6.75"
+          }, {
+            "label": "NA/SA",
+            "color": "##FF0000",
+            "value": "6.75"
+          }, {
+            "label": "OC",
+            "color": "##FF0000",
+            "value": "6.75"
+          }]
+        }, {
+          "label": "Transport",
+          "color": "##EE82EE",
+          "value": "22.5",
+          "tooltext": "Baby Products, $$valueK, $percentValue",
+          "category": [{
+            "label": "EU",
+            "color": "##EE82EE",
+            "value": "9.45",
+            "tooltext": "Bath & Grooming, $$valueK, $percentValue"
+          }, {
+            "label": "AF",
+            "color": "##EE82EE",
+            "value": "6.3"
+          }, {
+            "label": "AS",
+            "color": "##EE82EE",
+            "value": "6.75"
+          }, {
+            "label": "NA/SA",
+            "color": "##EE82EE",
+            "value": "6.75"
+          }, {
+            "label": "OC",
+            "color": "##EE82EE",
+            "value": "6.75"
+          }]
+        }, {
+          "label": "Batiments",
           "color": "#ccff66",
           "value": "30",
           "category": [{
-            "label": "Laptops",
+            "label": "EU",
             "color": "#ccff66",
             "value": "8.1"
           }, {
-            "label": "Televisions",
+            "label": "AF",
             "color": "#ccff66",
             "value": "10.5"
           }, {
-
-            "label": "SmartPhones",
+            "label": "AS",
+            "color": "#ccff66",
+            "value": "11.4"
+          }, {
+            "label": "NA/SA",
+            "color": "#ccff66",
+            "value": "11.4"
+          }, {
+            "label": "OC",
             "color": "#ccff66",
             "value": "11.4"
           }]
@@ -120,25 +224,8 @@
   topProductsChart.render();
 }
 
+//Food & {br}Beverages
+
  function update_pie(data_json) {
      // Le traitement des données d'émissions est réalisé ici
-     var data = google.visualization.arrayToDataTable([
-            ['Task', 'Hours per Day'],
-            ['Agriculture',     11],
-            ['Energie',      2],
-            ['Industrie',  2],
-            ['Batiments', 2],
-            ['Electricité & Chaleur',    7],
-            ['Énergie de fabrication/construction',    7],
-            ['Transport',    7]
-          ]);
-
-          var options = {
-            title: 'My Daily Activities',
-            pieHole: 0.4,
-          };
-
-          var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-          chart.draw(data, options);
  }
