f76e61b26c355ec7c3119cd30b310577141cf7fa
Michel Lecorsier <Michel.Lecorsier@grenoble-inp.org>
2020-09-25 15:24:13 +0200

    Begining of the map functions refactoring

--- scripts/map2.js
+++ scripts/map2.js
@@ -1,24 +1,24 @@
+/**
+ * Module permettant de générer une carte choroplèthe
+ * des émissions de gaz à effet de serre en fonction des
+ * pays considérés.
+ * 
+ * 2020 - Notre super équipe :-)
+ */
+
+const zoom = d3.zoom()
+    .scaleExtent([1, 8])
+    .on("zoom", zoomed);
+
 const width = 975;
 const height = 610;
-legendCellSize = 20,
-colors = ['#d4eac7', '#c6e3b5', '#b7dda2', '#a9d68f', '#9bcf7d', '#8cc86a', '#7ec157', '#77be4e', '#70ba45', '#65a83e', '#599537', '#4e8230', '#437029', '#385d22', '#2d4a1c', '#223815']
+const legendCellSize = 20;
+const colors = ['#d4eac7', '#c6e3b5', '#b7dda2', '#a9d68f', '#9bcf7d', '#8cc86a', '#7ec157', '#77be4e', '#70ba45', '#65a83e', '#599537', '#4e8230', '#437029', '#385d22', '#2d4a1c', '#223815']
 
-const svg = d3.select("#map").append("svg")
-    .attr("viewBox", [0, 0, width, height])
-    .on("click", reset);
-
-//start new code
-svg.append("svg:rect")
-    .attr("fill", "#1aa1d6")
-    .attr('height', height)
-    .attr('width', width);
-//end new code
-
-const g = svg.append("g");
-
-var cGroup = g.append("g");
-
+// bounds of the map (for clipping)
 const boundsMap = [70, 60]
+
+// Map projection parameters
 const projection = d3.geoNaturalEarth1()
     .scale(180)
     .translate([(width - boundsMap[0]) / 2 + boundsMap[0], (height - boundsMap[1]) / 2 + boundsMap[1]])
@@ -27,20 +27,152 @@ const path = d3.geoPath()
     .pointRadius(2)
     .projection(projection);
 
-// Dessin de la carte
-var promises = [];
-promises.push(d3.json("https://gist.githubusercontent.com/djdmsr/c8ed350bc46ae193767c4591bc133e0b/raw/4e94db2536d4008c72fb24fa3b244d77a5f1f17b/world-countries-no-antartica.json"));
+/**
+  * Init the map container with a legend, titles and countries drawn.
+  * @param {*} id_container 
+  * @param {*} id_map 
+  * @param {*} url_geojson 
+  */
+function init_map() {
+    // Create root svg element
+    const svg = d3.select("#map").append("svg")
+        .attr("id", "svg_zone")
+        .attr("viewBox", [0, 0, width, height])
+        .on("click", reset);
 
-Promise.all(promises).then(function(values) {
-    console.log(values);
-    world = values[0];
+    // Blue background (for sea?)
+    svg.append("svg:rect")
+        .attr("fill", "#1aa1d6")
+        .attr('height', height)
+        .attr('width', width);
 
-    //start new code
-    //compute min and max just for test. we will remove this part when we have these values in a js file
-    var year = 2005;
+    
+    var g = svg.append("g")
+                .attr("id", "g");
+
+    // cGroup = countries group
+    var cGroup = g.append("g")
+                    .attr("id", "cGroup");
+
+    // Drawing the map
+
+    // load geojson data
+    var promises = [];
+    promises.push(d3.json("https://gist.githubusercontent.com/djdmsr/c8ed350bc46ae193767c4591bc133e0b/raw/4e94db2536d4008c72fb24fa3b244d77a5f1f17b/world-countries-no-antartica.json"));
+
+    Promise.all(promises).then(function(values) {
+        // console.log(values);
+        world = values[0];
+        
+        // Draw countries
+        cGroup.append("g")
+            .attr("cursor", "pointer")
+            .selectAll("path")
+            .data(world.features)
+            .join("path")
+            .on("click", clicked)
+            .attr("d", path)
+            .attr("id", d => "code" + d.id)
+            .attr("fill", "gray")
+            .append("title")
+            .text(d => d.properties.name);
+    }, (error) => {
+        console.log(error); // erreur
+    });
+
+    // Draw clip rectangles
+    const clipRectangles = svg.append("g");
+    clipRectangles.append('svg:rect')
+        .attr('height', height + "px")
+        .attr('width', boundsMap[0] + 'px')
+        .attr('x', 0)
+        .attr('y', 0)
+        .style("fill", "#FFFFFF");
+
+    clipRectangles.append('svg:rect')
+        .attr('height', boundsMap[1] + "px")
+        .attr('width',  (width + 10) + 'px')
+        .attr('x', 0)
+        .attr('y', 0)
+        .style("fill", "#FFFFFF");
+
+    clipRectangles.append('svg:rect')
+        .attr('height', 10 + legendCellSize)
+        .attr('width',  (width + 10) + 'px')
+        .attr('x', 0)
+        .attr('y', height - (5 + legendCellSize))
+        .style("fill", "#FFFFFF");
+
+    // Ajout du titre
+    svg.append("text")
+        .attr("x", (width / 2))
+        .attr("y", 25)
+        .attr("text-anchor", "middle")
+        .style("fill", "#000000")
+        .style("font-weight", "300")
+        .style("font-size", "16px")
+        .text("Émissions de gaz à effet de serre dans le monde.");
+
+    svg.append("text")
+        .attr("x", (width / 2))
+        .attr("y", 50)
+        .attr("text-anchor", "middle")
+        .style("fill", "#000000")
+        .style("font-weight", "200")
+        .style("font-size", "12px")
+        .text("(source : Hannah Ritchie & Max Roser 2017 - CO₂ and Greenhouse Gas Emissions)");
+
+    svg.call(zoom);
+}
+
+
+function reset() {
+    const svg = d3.select("#svg_zone");
+    const cGroup = d3.select("#cGroup");
+    console.log(cGroup)
+    cGroup.transition().style("stroke", null);
+    svg.transition().duration(750).call(
+        zoom.transform,
+        d3.zoomIdentity,
+        d3.zoomTransform(svg.node()).invert([width / 2, height / 2])
+    );
+}
+    
+function clicked(event, d) {
+    const svg = d3.select("#svg_zone");
+    const cGroup = d3.select("#cGroup");
+    const [[x0, y0], [x1, y1]] = path.bounds(d);
+
+    event.stopPropagation();
+    cGroup.transition().style("stroke", null);
+    d3.select(this).transition().style("stroke", "red");
+    svg.transition().duration(750).call(
+        zoom.transform,
+        d3.zoomIdentity
+        .translate(width / 2, height / 2)
+        .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
+        .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
+        d3.pointer(event, svg.node())
+    );
+}
+    
+function zoomed(event) {
+    const {transform} = event;
+    const g = d3.select("#g");
+    g.attr("transform", transform);
+    g.attr("stroke-width", 1 / transform.k);
+}
+
+function update_legend(year) {
+    // update legend with new data
+
+    // Compute min/max values for the legend scale
     var min, max;
     var first = 0;
-    Object.keys(gas_complete_data).forEach(function(key, index) {
+
+    const svg = d3.select("#svg_zone");
+
+    Object.keys(gas_complete_data).forEach(function(key) {
         if (first == 0) {
             if (gas_complete_data[key][year] && gas_complete_data[key][year].total_ghg) {
                 min = max = gas_complete_data[key][year].total_ghg;
@@ -59,160 +191,63 @@ Promise.all(promises).then(function(values) {
             }
         }
     });
-    console.log(min, max);
-    //end new code
+    // console.log(min, max);
 
-    cGroup = g.append("g")
-        .attr("cursor", "pointer")
-        .selectAll("path")
-        .data(world.features)
-        .join("path")
-        .on("click", clicked)
-        .attr("d", path)
-        .attr("id", d => d.id)
-        .attr("fill", function(d) { 
-            //start new code
-            if (gas_complete_data[d.id] && gas_complete_data[d.id][year] && gas_complete_data[d.id][year].total_ghg)
-            {
-                console.log(gas_complete_data[d.id][year]);
-                return colors[Math.floor(colors.length * gas_complete_data[d.id][year].total_ghg/(max-min))];
-            } else {
-                //no data for this country or year
-                return "#999";
-            }
-            //end new code
-        });
+    // Draw legend
+    // Dessin de la légende
+    // TODO: Choisir coorrectement les couleurs de la légende
+    var legend = svg.append('g')
+        .attr('transform', 'translate(40, 250)');
 
-    cGroup.append("title")
-        .text(d => d.properties.name);
-}, (error) => {
-    console.log(error); // erreur
-});
+    legend.selectAll()
+        .data(d3.range(colors.length))
+        .enter().append('svg:rect')
+            .attr('height', legendCellSize + 'px')
+            .attr('width', legendCellSize + 'px')
+            .attr('x', 5)
+            .attr('y', d => d * legendCellSize)
+            .style("fill", d => colors[d]);
 
-// Draw clip rectangles
-const clipRectangles = svg.append("g");
-clipRectangles.append('svg:rect')
-    .attr('height', height + "px")
-    .attr('width', boundsMap[0] + 'px')
-    .attr('x', 0)
-    .attr('y', 0)
-    .style("fill", "#FFFFFF");
+    let legendScale = d3.scaleLinear().domain([min, max])
+        .range([0, colors.length * legendCellSize]);
+                
+    let legendAxis = legend.append("g")
+        .attr("class", "axis")
+        .call(d3.axisLeft(legendScale));
 
-clipRectangles.append('svg:rect')
-    .attr('height', boundsMap[1] + "px")
-    .attr('width',  (width + 10) + 'px')
-    .attr('x', 0)
-    .attr('y', 0)
-    .style("fill", "#FFFFFF");
-
-clipRectangles.append('svg:rect')
-    .attr('height', 10 + legendCellSize)
-    .attr('width',  (width + 10) + 'px')
-    .attr('x', 0)
-    .attr('y', height - (5 + legendCellSize))
-    .style("fill", "#FFFFFF");
-
-// Ajout du titre
-svg.append("text")
-    .attr("x", (width / 2))
-    .attr("y", 25)
-    .attr("text-anchor", "middle")
-    .style("fill", "#000000")
-    .style("font-weight", "300")
-    .style("font-size", "16px")
-    .text("Émissions de gaz à effet de serre dans le monde.");
-
-svg.append("text")
-    .attr("x", (width / 2))
-    .attr("y", 50)
-    .attr("text-anchor", "middle")
-    .style("fill", "#000000")
-    .style("font-weight", "200")
-    .style("font-size", "12px")
-    .text("(source : Hannah Ritchie & Max Roser 2017 - CO₂ and Greenhouse Gas Emissions)");
-
-// Dessin de la légende
-// TODO: Choisir coorrectement les couleurs de la légende
-// TODO: Change min/max
-var legend = svg.append('g')
-    .attr('transform', 'translate(40, 250)');
-
-legend.selectAll()
-    .data(d3.range(colors.length))
-    .enter().append('svg:rect')
+    legend.append('svg:rect')
+        .attr('y', legendCellSize + colors.length * legendCellSize)
         .attr('height', legendCellSize + 'px')
         .attr('width', legendCellSize + 'px')
         .attr('x', 5)
-        .attr('y', d => d * legendCellSize)
-        .style("fill", d => colors[d]);
-let min = 0
-let max = 160
-let legendScale = d3.scaleLinear().domain([min, max])
-    .range([0, colors.length * legendCellSize]);
-            
-let legendAxis = legend.append("g")
-    .attr("class", "axis")
-    .call(d3.axisLeft(legendScale));
+        .style("fill", "#999");
 
-legend.append('svg:rect')
-    .attr('y', legendCellSize + colors.length * legendCellSize)
-    .attr('height', legendCellSize + 'px')
-    .attr('width', legendCellSize + 'px')
-    .attr('x', 5)
-    .style("fill", "#999");
-
-legend.append("text")
-    .attr("x", 30)
-    .attr("y", 35 + colors.length * legendCellSize)
-    .style("font-size", "13px")
-    .style("color", "#000000")
-    .style("fill", "#000000")
-    .text("données non connues");
-
-
-function reset() {
-    cGroup.transition().style("stroke", null);
-    svg.transition().duration(750).call(
-        zoom.transform,
-        d3.zoomIdentity,
-        d3.zoomTransform(svg.node()).invert([width / 2, height / 2])
-    );
+    legend.append("text")
+        .attr("x", 30)
+        .attr("y", 35 + colors.length * legendCellSize)
+        .style("font-size", "13px")
+        .style("color", "#000000")
+        .style("fill", "#000000")
+        .text("données non connues");
 }
-    
-function clicked(event, d) {
-    const [[x0, y0], [x1, y1]] = path.bounds(d);
-    event.stopPropagation();
-    cGroup.transition().style("stroke", null);
-    d3.select(this).transition().style("stroke", "red");
-    svg.transition().duration(750).call(
-        zoom.transform,
-        d3.zoomIdentity
-        .translate(width / 2, height / 2)
-        .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
-        .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
-        d3.pointer(event, svg.node())
-    );
-}
-    
-function zoomed(event) {
-    const {transform} = event;
-    g.attr("transform", transform);
-    g.attr("stroke-width", 1 / transform.k);
-}
-    
     
 //start new code
-function updatemap(year) {
-    
-}
+// function update_data_map(year) {
+//     // call update_legend
+
+//     //start new code
+//     if (gas_complete_data[d.id] && gas_complete_data[d.id][year] && gas_complete_data[d.id][year].total_ghg)
+//     {
+//         console.log(gas_complete_data[d.id][year]);
+//         return colors[Math.floor(colors.length * gas_complete_data[d.id][year].total_ghg/(max-min))];
+//     } else {
+//         //no data for this country or year
+//         return "#999";
+//     }
+//     //end new code
+// }
 
 function setcolorcountry(year, id) {
     
 }
 //end new code
-
-const zoom = d3.zoom()
-    .scaleExtent([1, 8])
-    .on("zoom", zoomed);
-
-svg.call(zoom);
